4a90351683b78edd07ad18922e9c722eaaaefc02
Joris von Loghausen <joris@geenstijl.nl>
2017-03-07 17:06:14 +0100

    Variable rename Cache based on $partyRules instead of $_GET

--- render.php
+++ render.php
@@ -7,18 +7,19 @@ define('THEME_START_X', 113);
 define('PARTY_START_X', 638);
 define('PARTY_O_START_Y', 635);
 define('FONT_SIZE', 29);
+define('MAX_LINES', 6);
 define('IMG_HEAD', 'render_head.jpg');
 define('IMG_FOOT_OVERIG_PARTIJ', 'render_foot_overig_partij.jpg');
 define('IMG_FOOT_OVERIG_ZELF', 'render_foot_overig_zelf.jpg');
-define('IMG_FOOR_OVERIG_HEIGHT', 582);
+define('IMG_FOOT_OVERIG_HEIGHT', 582);
 define('IMG_FOOT_CLEAN', 'render_foot_clean.jpg');
-define('IMG_FOOR_CLEAN_HEIGHT', 348);
+define('IMG_FOOT_CLEAN_HEIGHT', 348);
 define('IMG_EEN_PARTIJ', 'render_een_partij.jpg');
 define('IMG_ZELF_STEMMEN', 'render_zelf_stemmen.jpg');
-define('TEXT_1', 'Ik stem zelf');
+define('TEXT_STEM_ZELF', 'Ik stem zelf');
 
-$ftL = dirname(__FILE__) . '/helvetica.ttf';
-$ftB = dirname(__FILE__). '/helvetica-bold.ttf';
+$font_light = dirname(__FILE__) . '/helvetica.ttf';
+$font_bold = dirname(__FILE__). '/helvetica-bold.ttf';
 
 $memcache = new Memcache();
 $memcache->connect('localhost', 11211) or die ("Could not connect");
@@ -26,17 +27,7 @@ $memcache->connect('localhost', 11211) or die ("Could not connect");
 header('Content-type: image/jpeg');
 
 include('pdata.php');
-$ip = array();
-
-$image_key = md5(http_build_query($_GET));
-if (count($_GET) == 1) {
-  $cachedImage = $memcache->get($image_key);
-  if ($cachedImage) {
-    $image = ImageCreateFromString(base64_decode($cachedImage));
-    ImageJpeg($image);
-    exit();
-  }
-}
+$partijRegels = array();
 
 foreach($_GET as $key => $value) {
   if (
@@ -61,63 +52,77 @@ foreach($_GET as $key => $value) {
     $zelfStemmen = true;
   }
 
-  $ip[$themes[$_GET['thema_'.$id]]] = ($zelfStemmen ? TEXT_1 : $parties[$_GET['keuze_'.$id]]);
+  $partijRegels[$themes[$_GET['thema_'.$id]]] = ($zelfStemmen ? TEXT_STEM_ZELF : $parties[$_GET['keuze_'.$id]]);
 }
 
 if(isset($_GET['keuze_overig']) && $_GET['keuze_overig'] != 0) {
-  $ip['overig'] = $parties[$_GET['keuze_overig']];
+  $partijRegels['overig'] = $parties[$_GET['keuze_overig']];
 }
 
 if($_GET['keuze_overig'] == 0) {
-  $ip['overig'] = 'self';
+  $partijRegels['overig'] = 'self';
 }
 
+
+// Show cached image if it exists
+$image_key = md5(http_build_query($partijRegels));
+$cachedImage = $memcache->get($image_key);
+if ($cachedImage) {
+  $image = ImageCreateFromString(base64_decode($cachedImage));
+  ImageJpeg($image);
+  exit();
+}
+
+
+$numPartijRegels = count($partijRegels);
+
 // Altijd zelf stemmen
-if(count($ip) == 0) {
+if($numPartijRegels == 0) {
   $rd = imagecreatefromjpeg(IMG_ZELF_STEMMEN);
-  imagettftext($rd, FONT_SIZE, 0, THEME_START_X, PARTY_O_START_Y, imagecolorallocate($rd, 0, 0, 0), $ftB, $p);
   imagejpeg($rd);
   imagedestroy($rd);
   exit;
 }
 
 // Altijd op 1 partij stemmen
-if(count($ip) == 1 && isset($ip['overig'])) {
+if($numPartijRegels == 1 && isset($partijRegels['overig'])) {
   $rd = imagecreatefromjpeg(IMG_EEN_PARTIJ);
-  imagettftext($rd, FONT_SIZE, 0, THEME_START_X, PARTY_O_START_Y, imagecolorallocate($rd, 0, 0, 0), $ftB, $ip['overig']);
+  imagettftext($rd, FONT_SIZE, 0, THEME_START_X, PARTY_O_START_Y, imagecolorallocate($rd, 0, 0, 0), $ftB, $partijRegels['overig']);
   imagejpeg($rd);
   imagedestroy($rd);
   exit;
 }
 
+// Wat doet dit?!
+/*
 $p = "";
 $a = true;
-foreach($ip as $v)
+foreach($partijRegels as $v)
 {
   if(empty($p)) $p = $v;
   if($v != $p) $a = false;
 }
+*/
 
-$numPartijRegels = count($ip);
-if (isset($ip['overig'])) {
+if (isset($partijRegels['overig'])) {
   $numPartijRegels--;
 }
 
-$rdH = IMG_MIN_HEIGHT+$numPartijRegels*TEXT_SPACING_Y+(isset($ip['overig']) ? IMG_FOOR_OVERIG_HEIGHT-IMG_FOOR_CLEAN_HEIGHT : 0);
+$rdH = IMG_MIN_HEIGHT+max($numPartijRegels, MAX_LINES)*TEXT_SPACING_Y+(isset($partijRegels['overig']) ? IMG_FOOT_OVERIG_HEIGHT-IMG_FOOT_CLEAN_HEIGHT : 0);
 
 $rd = imagecreatetruecolor(IMG_WIDTH, $rdH);
-$cBl = imagecolorallocate($rd, 0, 0, 0);
-$cWh = imagecolorallocate($rd, 255, 255, 255);
-imagefilledrectangle($rd,0,0,IMG_WIDTH,$rdH,$cWh);
+$colorBlack = imagecolorallocate($rd, 0, 0, 0);
+$colorWhite = imagecolorallocate($rd, 255, 255, 255);
+imagefilledrectangle($rd,0,0,IMG_WIDTH,$rdH,$colorWhite);
 
 $rd_hd = imagecreatefromjpeg(IMG_HEAD);
 imagecopymerge($rd,$rd_hd,0,0,0,0,imagesx($rd_hd),imagesy($rd_hd),100);
 
-if(isset($ip['overig']) && $ip['overig'] != 'self') {
+if(isset($partijRegels['overig']) && $partijRegels['overig'] != 'self') {
   $rd_ft1 = imagecreatefromjpeg(IMG_FOOT_OVERIG_PARTIJ);
   imagecopymerge($rd,$rd_ft1,0,$rdH-imagesy($rd_ft1),0,0,imagesx($rd_ft1),imagesy($rd_ft1),100);
-  imagettftext($rd, FONT_SIZE, 0, THEME_START_X-5, $rdH-400, $cBl, $ftB, $ip['overig']);
-} else if(isset($ip['overig']) && $ip['overig'] == 'self') {
+  imagettftext($rd, FONT_SIZE, 0, THEME_START_X-5, $rdH-400, $colorBlack, $font_bold, $partijRegels['overig']);
+} else if(isset($partijRegels['overig']) && $partijRegels['overig'] == 'self') {
   $rd_ft3 = imagecreatefromjpeg(IMG_FOOT_OVERIG_ZELF);
   imagecopymerge($rd,$rd_ft3,0,$rdH-imagesy($rd_ft3),0,0,imagesx($rd_ft3),imagesy($rd_ft3),100);
 } else {
@@ -125,18 +130,18 @@ if(isset($ip['overig']) && $ip['overig'] != 'self') {
   imagecopymerge($rd,$rd_ft2,0,$rdH-imagesy($rd_ft2),0,0,imagesx($rd_ft2),imagesy($rd_ft2),100);
 }
 
-$i = TEXT_START_Y;
-$n = 0;
-foreach($ip as $theme => $party)
+$pos_Y = TEXT_START_Y;
+$rulesPrinted = 0;
+foreach($partijRegels as $theme => $party)
 {
- if($n>6) break; // again? max 6 items?
+ if($rulesPrinted>MAX_LINES) break; // again? max 6 items?
  if($theme=='overig') continue;
  
- imagettftext($rd, FONT_SIZE, 0, THEME_START_X, $i, $cBl, $ftL, $theme);
- imagettftext($rd, FONT_SIZE, 0, PARTY_START_X, $i, $cBl, $ftB, $party);
+ imagettftext($rd, FONT_SIZE, 0, THEME_START_X, $pos_Y, $colorBlack, $font_light, $theme);
+ imagettftext($rd, FONT_SIZE, 0, PARTY_START_X, $pos_Y, $colorBlack, $font_bold, $party);
  
- $i+= TEXT_SPACING_Y;
- $n++;
+ $pos_Y+= TEXT_SPACING_Y;
+ $rulesPrinted++;
 }
 
 ob_start();
